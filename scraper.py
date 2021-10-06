8b75079235b6b3d1c29d173ab873f820834dcb9f
sedadenboer <sedadenboer21@gmail.com>
2021-10-06 13:20:10 +0200

    Finalize scraper.py

--- scraper.py
+++ scraper.py
@@ -21,13 +21,10 @@ def main(output_file_name, start_year, end_year):
     """
     Info
     """
-    # random years to test program with
-    start_year = 2014
-    end_year = 2018
 
     # calculate the amount of years specified in the time interval
     year_interval = end_year - start_year + 1
-    print('year interval', year_interval)
+    print(year_interval)
 
     # initialize
     df_list = []
@@ -36,7 +33,7 @@ def main(output_file_name, start_year, end_year):
 
     while True:
         # changeable page url, based on what page we're looking at
-        page_url = "https://www.imdb.com/search/title/?title_type=feature&release_date=1930-01-01,2020-01-01&num_votes=5000,&sort=user_rating,desc&start=" + str(page) + "&view=advanced"
+        page_url = f"https://www.imdb.com/search/title/?title_type=feature&release_date={start_year}-01-01,{end_year + 1}-01-01&num_votes=5000,&sort=user_rating,desc&start=" + str(page) + "&view=advanced"
 
         # load website with BeautifulSoup
         html = simple_get(page_url)
@@ -47,36 +44,43 @@ def main(output_file_name, start_year, end_year):
 
         # mask values that are not in the specified year interval range and put dataframes in a list
         movies_df = movies_df[np.logical_and(movies_df.year >= start_year, movies_df.year <= end_year)]
+        print(movies_df)
         df_list.append(movies_df)
 
         # tracking counts of movies in every year and adding these up as we're looping
         counts = movies_df['year'].value_counts().sort_index(ascending=False)
         total_counts = total_counts.add(counts, fill_value=0).sort_index(ascending=False)
-        print(total_counts)
-
-        # amount of movies retrieved of a certain year has to be greater than or equal to 5
-        threshold = total_counts.value_counts() < 2 # for now I set this value to 1, to make testing the code easier
-        print(threshold)
-
-        # print(movies_df)
-
-        # condition 1: movies from every year of the time interval, condition 2: at least 5 movies for every year
-        counted_years = total_counts.size
-        if counted_years == year_interval and not threshold.any():
-            break
         
+        # condition 1: movies from every year of the time interval
+        counted_years = total_counts.size
+        # condition 2: at least 5 movies for every year
+        threshold = total_counts >= 5
+
+        # print statements for testing
+        print(total_counts)
+        print(threshold)
+        print('counted_years', counted_years)
+
+        # number of years in db should be equal to specified year interval and .all() checks if threshold is True for all years
+
+        if threshold.all() and threshold.size == year_interval:
+            break
+
         # change pagenumber to retrieve movie data from multiple pages if necessary
         page += 50
-        
+        print("NUMBER OF YEARS", year_interval, threshold.size)
+        print(page)
 
-    print('size', total_counts.size)
+    # print('year interval', year_interval)
+    # print('size', total_counts.size)
 
     # join all gathered dataframes from the loop into one big dataframe
     all_movies_df = pd.concat(df_list).sort_values(['year', 'rating'], ascending=False)
+
     print(all_movies_df)
 
     # save results to output file
-    # all_movies_df.to_csv(output_file_name, index=False)
+    all_movies_df.to_csv(output_file_name, index=False)
 
 
 def extract_movies(dom):
@@ -93,13 +97,18 @@ def extract_movies(dom):
         year_unstripped = movie.find('span', class_ = 'lister-item-year text-muted unbold').string
         year = int(re.sub("[^0-9]", "", year_unstripped))
         actor = ';'.join([a.string for a in movie.find('p', class_ = '').find_all('a')[1:]])
-        runtime = movie.find('span', class_ = 'runtime').string.strip('min')
+        runtime = movie.find('span', class_ = 'runtime')
+        if runtime:
+            runtime = runtime.string.strip('min')
+        else:
+            runtime = '-'
+
         url = ('https://www.imdb.com/' + movie.a['href'])
 
-        # put movie information together in a list
-        movie_data = [title, rating, year, actor, runtime, url]
+        # put movie information together in a dictionary
+        movie_data = {'title': title, 'rating': rating, 'year': year, 'actors': actor, 'runtime': runtime, 'url': url}
 
-        # put the lists with movie information in a general list for all movies
+        # put the dictionaries with movie information in a general list for all movies
         movies_list.append(movie_data)
 
     # make a dataframe out of the lists with movie information
